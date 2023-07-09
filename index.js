454b5aa9f41ddb41ccd1848781138d8d3e710827
Ankitp9800 <82386308+Ankitp9800@users.noreply.github.com>
2023-07-10 00:38:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bp = require("body-parser")
 const app= express();
+const path = require('path');
 const PORT = 8080;
 
 require("./db/connection");
 app.use(cors("*"));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: false }));;
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use("/v1", require("./routes/router.js"));
 
