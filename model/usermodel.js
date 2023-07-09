498910d6282b8107f7681c4cde4a6bb0247535c5
Ankitp9800 <82386308+Ankitp9800@users.noreply.github.com>
2023-07-09 13:27:15 +0530

    Add files via upload

---
+++ model/usermodel.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const bcrypt = require('bcrypt')
+const autoIncrement = require('mongoose-auto-increment');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    type: Number,
+    default: null
+    // required: true,
+  },
+  password: {
+    type: String,
+    // required: true,
+    // minlength: 8,
+    // match: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+  },
+  email_id: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  user_name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    // validate: {
+    //   validator: function (value) {
+    //     return /^\+\d{1,3}\s?\d{6,14}$/.test(value)
+    //   },
+    //   message: (props) =>
+    //     `${props.value} is not a valid mobile number with country code!`,
+    // },
+  },
+  isProfilePublic: {
+    type: Boolean,
+    default: true,
+  },
+  followers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  blockedUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+})
+
+// userSchema.plugin(autoIncrement, { model: 'User', field: 'user_id' })
+userSchema.statics.getNextOrder = async function () {
+  const user = await this.countDocuments();
+  return user ? user+1 : 1;
+};
+userSchema.pre("save", async function (next) {
+  const doc = this;
+  if (doc.isNew) {
+    doc.user_id = await this.constructor.getNextOrder();
+  }
+  next();
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12)
+  }
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ model/usersPost.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  media: {
+    type: String
+  },
+  public: {
+    type: Boolean,
+    default: true
+  },
+  hashtags: [{
+    type: String
+  }],
+  friendTags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    text: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    subComments: [{
+      text: {
+        type: String,
+        required: true
+      },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      }
+    }]
+  }], 
+  likes: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  }]
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
